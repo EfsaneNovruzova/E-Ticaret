b02ab5c2a610cfe7e157dfd6a0344c822f2ad7f5
EfsaneNovruzova <novruzovafsan88@gmail.com>
2025-07-02 00:08:14 +0400

    Create/DbContext

--- src/Presentation/E-Ticaret.WebApi/Program.cs
+++ src/Presentation/E-Ticaret.WebApi/Program.cs
@@ -1,9 +1,15 @@
+using E_Ticaret.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-
+builder.Services.AddDbContext<E_TicaretDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
